4a5d56673e3eaa4821f7390b7344baab47fcb365
Riccardo <riki-95@hotmail.it>
2018-09-10 22:32:51 +0200

    First commit

---
+++ social_contagion.py
@@ -0,0 +1,102 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+
+file = None  # open('out', 'w')
+a = 1
+b = 100000
+
+
+def create_bitcoin_graph(g):
+    with open('bitcoin.csv') as f:
+        for row in f:
+            s = row.split(',')
+            g.add_edge(s[0], s[1], weight=int(s[2]))
+
+
+def from_bools_to_colors(contagion_list):
+    for k, v in contagion_list.items():
+        if v:
+            contagion_list[k] = 'red'
+        else:
+            contagion_list[k] = 'blue'
+    return contagion_list
+
+
+# Saves graphs into data folder
+def draw_graph(g, day, layout):
+    colors = from_bools_to_colors(nx.get_node_attributes(g, 'contagion'))
+    nx.draw(g, node_color=list(colors.values()), pos=layout, node_size=50, font_size=6, font_color='w', arrowsize=3)
+    plt.draw()
+    plt.savefig('data/' + str(day) + '.png', dpi=500)
+    plt.close()
+
+
+# First Iteration of contagion
+def initialize_graph(g, percentage):
+    n = len(g) * percentage
+    for i in range(int(n)):
+        v = random.choice(list(g.node.keys()))
+        g.node[v]['contagion'] = True
+
+
+def main():
+    g = nx.DiGraph()
+    create_bitcoin_graph(g)
+    # g = nx.scale_free_graph(2000)
+    # g = nx.fast_gnp_random_graph(500,0.075)
+    g = g.to_undirected()
+    layout = nx.spring_layout(g)
+    nx.set_node_attributes(g, False, 'contagion')  # Setta a tutti i parametri contagion il valore false
+    # print(nx.get_node_attributes(g, "contagion"))
+    draw_graph(g, 0, layout)
+
+    initialize_graph(g, 0.1)
+    draw_graph(g, 1, layout)
+
+    days = 5
+    rr = 1
+    bb = len(g.nodes) - 1
+    for day in range(days):
+        print('Day: ', day, file=file)
+        for n in g.nodes():
+            print('\tnode=', n, file=file)
+            nn_number = len(list(set(g.edges(n))))
+            print('\t\tVicini lista:', list(set(g.edges(n))), file=file)
+            print('\t\tVicini: ', nn_number, file=file)
+            if nn_number == 0:
+                continue
+
+            blues = 0
+            for frm, to in list(set(g.edges(n))):
+                if not g.node[to]['contagion']:
+                    blues += 1
+            print('\t\tblues=', blues, file=file)
+            p = blues / nn_number
+            print('\t\tp=', p, file=file)
+            not_contagion_percentage = p * nn_number * a
+            print('\t\tcontagion=', not_contagion_percentage, file=file)
+            contagion_percentage = (1 - p) * nn_number * b
+            print('\t\tcontagion=', contagion_percentage, file=file)
+            if contagion_percentage > not_contagion_percentage:
+                g.node[n]['contagion'] = True
+                rr += 1
+                bb -= 1
+            print('\tblue ', bb, 'red ', rr, file=file)
+        draw_graph(g, day + 2, layout)
+
+    plt.close()
+
+    infected = []
+    not_infected = []
+    for n in g.nodes:
+        if g.node[n]['contagion']:
+            infected.append(n)
+        else:
+            not_infected.append(n)
+    print('Infected nodes: \n\t', infected, file=file)
+    print('Not-Infected nodes: \n\t', not_infected, file=file)
+
+
+if __name__ == '__main__':
+    main()
